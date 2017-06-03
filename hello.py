dbcc1e15e90f87fa38efb7e101b8c7a6e0863373
andyfriedl <andyfriedl@gmail.com>
2017-06-02 21:34:05 -0500

    sma;; change

--- hello.py
+++ hello.py
@@ -26,7 +26,7 @@ def main():
     print 'here'
     # Command line args are in sys.argv[1], sys.argv[2] ...
     # sys.argv[0] is the script name itself and can be ignored
-
+# cool
 # Standard boilerplate to call the main() function to begin
 # the program.
 if __name__ == '__main__':
